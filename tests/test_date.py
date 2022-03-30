28da2743c474eef233b7c33637754dd36c488b28
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-03-30 13:53:18 +0300

    [PATCH] Added missing functionality and tests

--- tests/test_date.py
+++ tests/test_date.py
@@ -4,20 +4,27 @@ import pytest
 
 from unit_testing import date
 
+
 @pytest.fixture
 def default_date():
+    """Creates a default date.Date object for testing"""
+
     return date.Date()
 
 
-def test_default_attributes(default_date):
+def test_date_default_attributes(default_date):
+    """Tests the default state of the date object"""
+
     assert default_date.day == 1
     assert default_date.month == 1
     assert default_date.year == 1900
 
 
-def test_default_methods(default_date):
-    assert default_date.leap_year() == False
-    
+def test_date_default_methods(default_date):
+    """Tests the methods on a default date object"""
+
+    assert default_date.leap_year() is False
+
     default_date.next_day()
 
     assert default_date.day == 2
@@ -25,17 +32,30 @@ def test_default_methods(default_date):
     assert default_date.year == 1900
 
 
-def test_year_change():
+def test_date_next_day(default_date):
+    d1 = date.Date(30, 3, 2022)
+    default_date.next_day(previous=d1)
+
+    assert default_date.day == 31
+    assert default_date.month == 3
+    assert default_date.year == 2022
+
+
+def test_date_year_change():
+    """Tests if year change is correctly implemented"""
+
     d = date.Date(31, 12, 1999)
     d.next_day()
 
     assert d.day == 1
     assert d.month == 1
     assert d.year == 2000
-    assert d.leap_year() == True
+    assert d.leap_year() is True
 
 
-def test_month_change():
+def test_date_month_change():
+    """Tests if month change is correctly implemented"""
+
     d = date.Date(28, 2, 2001)
     d.next_day()
 
@@ -44,27 +64,98 @@ def test_month_change():
     assert d.year == 2001
 
 
-def test_date_error():
+def test_date_day_error():
+    """Tests that invalid days raise an error"""
+
     with pytest.raises(ValueError):
-        d = date.Date(31, 2, 2020)
+        _ = date.Date(31, 2, 2020)
 
 
-def test_month_error():
+def test_date_month_error():
+    """Tests that invalid months raise an error"""
+
     with pytest.raises(ValueError):
-        d = date.Date(7, 21, 1998)
+        _ = date.Date(7, 21, 1998)
 
 
-def test_print(default_date):
-    assert default_date.print() is None
+def test_date_print(default_date, capsys):
+    """'Tests' the printing functionality"""
+
+    default_date.print()
+    captured = capsys.readouterr()
+    assert captured.out == '1/1/1900\n'
 
 
-def test_leap_year():
+def test_date_comparisons():
+    d1 = date.Date(1, 1, 2000)
+    d2 = date.Date(6, 1, 2000)
+    d3 = date.Date(24, 12, 1999)
+    d4 = date.Date(2, 2, 2000)
+    d5 = date.Date(1, 1, 2000)
+
+    assert d1 < d2
+    assert d2 > d1
+    assert d1 <= d2
+    assert d2 >= d1
+
+    assert d1 > d3
+    assert d3 < d1
+    assert d1 >= d3
+    assert d3 <= d1
+
+    assert d1 < d4
+    assert d4 > d1
+    assert d1 <= d4
+    assert d4 >= d1
+
+    assert d1 == d5
+    assert d1 <= d5
+    assert d1 >= d5
+
+    assert (d1 < None) is False
+    assert (d1 > None) is False
+    assert (d1 <= None) is False
+    assert (d1 >= None) is False
+    assert (d1 == 'foo') is False
+
+
+def test_date_leap_year():
+    """Tests that leap years are correctly implemented"""
+
     d1 = date.Date(1, 1, 2000)
     d2 = date.Date(1, 1, 2100)
     d3 = date.Date(1, 1, 2004)
     d4 = date.Date(1, 1, 2001)
 
-    assert d1.leap_year() == True
-    assert d2.leap_year() == False
-    assert d3.leap_year() == True
-    assert d4.leap_year() == False
+    assert d1.leap_year() is True
+    assert d2.leap_year() is False
+    assert d3.leap_year() is True
+    assert d4.leap_year() is False
+
+
+def test_date_days_between():
+    """Tests calculating difference in days betweeen dates"""
+
+    d1 = date.Date(24, 1, 1998)
+    d2 = date.Date(31, 1, 1998)
+    d3 = date.Date(5, 2, 1998)
+    d4 = date.Date(24, 1, 2005)
+
+    assert d1.days_between(d2) == 7
+    assert d1.days_between(d3) == 12
+    assert d1.days_between(d4) == 2557
+    assert d4.days_between(d1) == 2557
+
+
+def test_date_days_since_new_year():
+    d = date.Date(31, 1, 2022)
+
+    assert d.days_since_new_year() == 30
+
+
+def test_week_later(default_date):
+    new_date = date.week_later(default_date)
+
+    assert new_date.day == 8
+    assert new_date.month == 1
+    assert new_date.year == 1900
--- unit_testing/date.py
+++ unit_testing/date.py
@@ -2,50 +2,118 @@
 
 from typing import Optional
 
+
 class Date:
+    """Date implementation"""
+
     def __init__(self, day: Optional[int] = None, month: Optional[int] = None, year: Optional[int] = None):
-        self.year = year if year is not None else 1900
-        self.month = month if month is not None else 1
-        self.day = day if day is not None else 1
+        """Initialise Date object"""
+
+        self.year: int = year if year is not None else 1900
+        self.month: int = month if month is not None else 1
+        self.day: int = day if day is not None else 1
 
     @property
-    def day(self):
+    def day(self) -> int:
+        """Getter for days"""
+
         return self._day
 
     @day.setter
-    def day(self, other):
+    def day(self, other: int):
+        """Setter for days"""
+
         if other > self.days_in_month():
             raise ValueError(f"{other} is not a valid day count for the given month")
         self._day = other
 
     @property
-    def month(self):
+    def month(self) -> int:
+        """Getteer for months"""
+
         return self._month
 
     @month.setter
-    def month(self, other):
+    def month(self, other: int):
+        """Setter for months"""
+
         if not 1 <= other <= 12:
             raise ValueError(f"{other} is not a valid month")
         self._month = other
 
     @property
-    def year(self):
+    def year(self) -> int:
+        """Getter for years"""
+
         return self._year
 
     @year.setter
-    def year(self, other):
+    def year(self, other: int):
+        """Setter for years"""
+
         self._year = other
 
-    def days_in_month(self):
+    def __str__(self) -> str:
+        """String representation for date"""
+
+        return f"{self.day}/{self.month}/{self.year}"
+
+    def __gt__(self, other):
+        """Implements greater than for Date objects"""
+
+        if isinstance(other, Date):
+            if self.year > other.year:
+                return True
+            if self.year == other.year and self.month > other.month:
+                return True
+            if self.year == other.year and self.month == other.month and self.day > other.day:
+                return True
+        return False
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __lt__(self, other):
+        """Implements less than for Date objects"""
+
+        if isinstance(other, Date):
+            return not self >= other
+        return False
+
+    def __eq__(self, other):
+        """Checks for date object equality"""
+
+        if isinstance(other, Date):
+            return (
+                self.year == other.year
+                and self.month == other.month
+                and self.day == other.day
+            )
+        return False
+
+    def days_in_month(self) -> int:
+        """Count the days in a given month"""
+
         max_days = 31
         if self.month == 2:
             max_days = 28
+            if self.leap_year():
+                max_days += 1
         elif self.month in {4, 6, 9, 11}:
             max_days = 30
         return max_days
 
-    def next_day(self):
-        
+    def next_day(self, previous: Optional['Date'] = None):
+        """Move to the next day"""
+
+        if previous is not None:
+            self.year = previous.year
+            self.month = previous.month
+            self.day = previous.day
+
         if self.day + 1 > self.days_in_month():
             if self.month + 1 > 12:
                 self.year += 1
@@ -57,16 +125,49 @@ class Date:
             self.day += 1
 
     def print(self):
+        """Prints the current day"""
+
         print(self)
 
-    def __str__(self):
-        return f"{self.day}/{self.month}/{self.year}"
-
-    def leap_year(self):
+    def leap_year(self) -> bool:
+        """Check if the current year is a leap year"""
 
         if self.year % 400 == 0:
             return True
         if self.year % 100 == 0:
             return False
-        
+
         return self.year % 4 == 0
+
+    def days_between(self, other: 'Date') -> int:
+        """Returns the number of days between two given date objects"""
+
+        days = 0
+
+        destination = self
+        start = Date(other.day, other.month, other.year)
+        if self < other:
+            destination = other
+            start = Date(self.day, self.month, self.year)
+
+        while start < destination:
+            start.next_day()
+            days += 1
+            print(start, destination)
+
+        return days
+
+    def days_since_new_year(self) -> int:
+        """Returns the number of days since new year"""
+
+        new_year = Date(1, 1, self.year)
+        return self.days_between(new_year)
+
+
+def week_later(date: Date) -> Date:
+    """Returns a new Date object a week later from the given date"""
+
+    new_date = Date(date.day, date.month, date.year)
+    for _ in range(7):
+        new_date.next_day()
+    return new_date
