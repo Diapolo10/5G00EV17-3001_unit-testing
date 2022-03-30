651f888cb264520fca2c2d8af25ab583877aad3a
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-03-30 11:34:52 +0300

    [PATCH] More unit tests and fixes

--- tests/test_default.py
+++ tests/test_default.py
@@ -9,7 +9,7 @@ def default_date():
     return date.Date()
 
 
-def test_default_atttributes(default_date):
+def test_default_attributes(default_date):
     assert default_date.day == 1
     assert default_date.month == 1
     assert default_date.year == 1900
@@ -23,3 +23,32 @@ def test_default_methods(default_date):
     assert default_date.day == 2
     assert default_date.month == 1
     assert default_date.year == 1900
+
+
+def test_year_change():
+    d = date.Date(31, 12, 1999)
+    d.next_day()
+
+    assert d.day == 1
+    assert d.month == 1
+    assert d.year == 2000
+    assert d.leap_year() == True
+
+
+def test_month_change():
+    d = date.Date(28, 2, 2001)
+    d.next_day()
+
+    assert d.day == 1
+    assert d.month == 3
+    assert d.year == 2001
+
+
+def test_date_error():
+    with pytest.raises(ValueError):
+        d = date.Date(31, 2, 2020)
+
+
+def test_month_error():
+    with pytest.raises(ValueError):
+        d = date.Date(7, 21, 1998)
--- unit_testing/date.py
+++ unit_testing/date.py
@@ -3,10 +3,10 @@
 from typing import Optional
 
 class Date:
-    def __init__(self, day: Optional[int]=None, month: Optional[int]=None, year: Optional[int]=None):
-        self.day = day if day is not None else 1
-        self.month = month if month is not None else 1
+    def __init__(self, day: Optional[int] = None, month: Optional[int] = None, year: Optional[int] = None):
         self.year = year if year is not None else 1900
+        self.month = month if month is not None else 1
+        self.day = day if day is not None else 1
 
     @property
     def day(self):
@@ -14,6 +14,8 @@ class Date:
 
     @day.setter
     def day(self, other):
+        if other > self.days_in_month():
+            raise ValueError(f"{other} is not a valid day count for the given month")
         self._day = other
 
     @property
@@ -22,6 +24,8 @@ class Date:
 
     @month.setter
     def month(self, other):
+        if not 1 <= other <= 12:
+            raise ValueError(f"{other} is not a valid month")
         self._month = other
 
     @property
@@ -32,23 +36,25 @@ class Date:
     def year(self, other):
         self._year = other
 
-    def next_day(self):
-        
+    def days_in_month(self):
         max_days = 31
         if self.month == 2:
             max_days = 28
         elif self.month in {4, 6, 9, 11}:
             max_days = 30
+        return max_days
+
+    def next_day(self):
         
-        if self.day + 1 > max_days:
-            self.month += 1
+        if self.day + 1 > self.days_in_month():
+            if self.month + 1 > 12:
+                self.year += 1
+                self.month = 1
+            else:
+                self.month += 1
             self.day = 1
         else:
             self.day += 1
-
-        if self.month > 12:
-            self.month = 1
-            self.year += 1
 
     def print(self):
         print(self)
