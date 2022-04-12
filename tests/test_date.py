344c79af3fe037bf2bd6f0125ef3fe101d0f35c4
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-04-12 11:05:44 +0300

    [PATCH] Made unit test defaults use constants

--- tests/test_date.py
+++ tests/test_date.py
@@ -4,6 +4,10 @@ import pytest
 
 from unit_testing import date
 
+DEFAULT_DAY = 1
+DEFAULT_MONTH = 1
+DEFAULT_YEAR = 1900
+
 
 @pytest.fixture
 def default_date():
@@ -15,9 +19,9 @@ def default_date():
 def test_date_default_attributes(default_date):
     """Tests the default state of the date object"""
 
-    assert default_date.day == 1
-    assert default_date.month == 1
-    assert default_date.year == 1900
+    assert default_date.day == DEFAULT_DAY
+    assert default_date.month == DEFAULT_MONTH
+    assert default_date.year == DEFAULT_YEAR
 
 
 def test_date_default_methods(default_date):
@@ -27,12 +31,14 @@ def test_date_default_methods(default_date):
 
     default_date.next_day()
 
-    assert default_date.day == 2
-    assert default_date.month == 1
-    assert default_date.year == 1900
+    assert default_date.day == DEFAULT_DAY+1
+    assert default_date.month == DEFAULT_MONTH
+    assert default_date.year == DEFAULT_YEAR
 
 
 def test_date_next_day(default_date):
+    """Tests setting the 'previous'-parameter for Date.next_day"""
+
     d1 = date.Date(30, 3, 2022)
     default_date.next_day(previous=d1)
 
@@ -83,10 +89,12 @@ def test_date_print(default_date, capsys):
 
     default_date.print()
     captured = capsys.readouterr()
-    assert captured.out == '1/1/1900\n'
+    assert captured.out == f'{DEFAULT_DAY}/{DEFAULT_MONTH}/{DEFAULT_YEAR}\n'
 
 
 def test_date_comparisons():
+    """Tests comparison operators for Date objects"""
+
     d1 = date.Date(1, 1, 2000)
     d2 = date.Date(6, 1, 2000)
     d3 = date.Date(24, 12, 1999)
@@ -148,14 +156,18 @@ def test_date_days_between():
 
 
 def test_date_days_since_new_year():
+    """Tests calculating number of days since the last new year"""
+
     d = date.Date(31, 1, 2022)
 
     assert d.days_since_new_year() == 30
 
 
 def test_week_later(default_date):
+    """Tests calculating the data a week later"""
+
     new_date = date.week_later(default_date)
 
-    assert new_date.day == 8
-    assert new_date.month == 1
-    assert new_date.year == 1900
+    assert new_date.day == DEFAULT_DAY+7
+    assert new_date.month == DEFAULT_MONTH
+    assert new_date.year == DEFAULT_YEAR
