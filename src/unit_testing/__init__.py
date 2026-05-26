862c466bbfba6416a547eab75c1f93e9d3ed16b1
Lari Liuhamo <lari.liuhamo@gmail.com>
2026-05-27 02:20:06 +0300

    feat: Added dynamic versioning and cleaned up the GitHub Actions files

--- src/unit_testing/__init__.py
+++ src/unit_testing/__init__.py
@@ -1,1 +1,8 @@
 """Basic code for showcasing unit testing."""
+
+import importlib
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
