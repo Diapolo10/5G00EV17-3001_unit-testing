ca7bf0c555c035d79268ac3421887d13860b108e
Lari Liuhamo <lari.liuhamo@gmail.com>
2026-06-01 21:31:19 +0300

    fix: Formatter changes

--- src/unit_testing/date.py
+++ src/unit_testing/date.py
@@ -15,18 +15,18 @@ MAX_DAYS_IN_MONTH = 31
 class Months(IntEnum):
     """Enum for months."""
 
-    JANUARY   = 1
-    FEBRUARY  = 2
-    MARCH     = 3
-    APRIL     = 4
-    MAY       = 5
-    JUNE      = 6
-    JULY      = 7
-    AUGUST    = 8
+    JANUARY = 1
+    FEBRUARY = 2
+    MARCH = 3
+    APRIL = 4
+    MAY = 5
+    JUNE = 6
+    JULY = 7
+    AUGUST = 8
     SEPTEMBER = 9
-    OCTOBER   = 10
-    NOVEMBER  = 11
-    DECEMBER  = 12
+    OCTOBER = 10
+    NOVEMBER = 11
+    DECEMBER = 12
 
 
 class Date:
